8c0b97507aa1983eedca481e0b6f50d75f3fddba
theodorous.yuvens <theodorus_beatayuvens@aeon.co.id>
2021-03-17 17:39:13 +0700

    make simple layout for quiz

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,108 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(Quizzler());
 }
 
-class MyApp extends StatelessWidget {
+class Quizzler extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
   }
 }
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10),
+            child: Center(
+              child: Text(
+                'This is where the question text will go',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15),
+            child: FlatButton(
+              color: Colors.green,
+              child: Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+              onPressed: () {
+                // The user picked true
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15),
+            child: FlatButton(
+              color: Colors.red,
+              child: Text(
+                'False',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+              onPressed: () {
+                // The user picked false
+              },
+            ),
+          ),
+        ),
+        // TODO: Add a Row here as your score keeper
+      ],
+    );
+  }
+}
+
+/*
+Question('Some cats are actually allergic to humans', true),
+Question('You can lead a cow down stairs but not up stairs.', false),
+Question('Approximately one quarter of human bones are in the feet.', true),
+Question('A slug\'s blood is green.', true),
+Question('Buzz Aldrin\'s mother\'s maiden name was \"Moon\".', true),
+Question('It is illegal to pee in the Ocean in Portugal.', true),
+Question('No piece of square dry paper can be folded in half more than 7 times.', false),
+Question('In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.', true),
+Question('The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.', false),
+Question('The total surface area of two human lungs is approximately 70 square metres.', true),
+Question('Google was originally called \"Backrub\".', true),
+Question('Chocolate affects a dog\'s heart and nervous system; a few ounces are enough to kill a small dog.', true),
+Question('In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.', true),
+ */
