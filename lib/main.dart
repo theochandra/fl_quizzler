a1c2a120092a3a012866fd222d553847a42ac27f
theodorous.yuvens <theodorus_beatayuvens@aeon.co.id>
2021-03-17 17:53:27 +0700

    add list of icon to track scores

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,18 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+
+  List<Icon> scoresKeeper = [
+    Icon(
+      Icons.check,
+      color: Colors.green,
+    ),
+    Icon(
+      Icons.close,
+      color: Colors.red,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -63,6 +75,14 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 // The user picked true
+                setState(() {
+                  scoresKeeper.add(
+                    Icon(
+                      Icons.check,
+                      color: Colors.green,
+                    ),
+                  );
+                });
               },
             ),
           ),
@@ -85,7 +105,9 @@ class _QuizPageState extends State<QuizPage> {
             ),
           ),
         ),
-        // TODO: Add a Row here as your score keeper
+        Row(
+          children: scoresKeeper,
+        )
       ],
     );
   }
